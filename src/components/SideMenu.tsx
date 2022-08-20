8ca952340c1f9faef99cb3515a8639cb802b256d
Felipe Salomão <felipe@directon.com.br>
2022-08-20 18:29:15 -0300

    feat(SideMenu): add logout and logo items

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,16 +1,45 @@
-import { BellIcon, HomeIcon, SettingsIcon } from "./icons";
+import { BellIcon, HomeIcon, LogoutIcon, SettingsIcon } from "./icons";
 import { SideMenuItem } from "./SideMenuItem";
+import { Logo } from "./Logo";
 
 export const SideMenu = () => {
   return (
-    <aside>
-      <ul>
+    <aside
+      className={`
+    flex flex-col
+    `}
+    >
+      <div
+        className={`
+        flex items-center justify-center
+        bg-gradient-to-r from-indigo-500 to-purple-800
+        h-20 w-20
+        `}
+        >
+        <Logo />
+      </div>
+      <ul
+        className={`
+      flex-grow
+      `}
+      >
         <SideMenuItem url="/" text="Home" icon={HomeIcon} />
         <SideMenuItem url="/settings" text="Settings" icon={SettingsIcon} />
         <SideMenuItem
           url="/notifications"
           text="Notifications"
           icon={BellIcon}
+        />
+      </ul>
+      <ul>
+        <SideMenuItem
+          onClick={() => { console.log('logout') }}
+          text="Logout"
+          icon={LogoutIcon}
+          className={`
+          text-red-600
+          hover:bg-red-400 hover:text-white
+          `}
         />
       </ul>
     </aside>
--- src/components/SideMenuItem.tsx
+++ src/components/SideMenuItem.tsx
@@ -1,19 +1,40 @@
 import Link from "next/link";
 
 interface ISideMenuItemProps {
-  url: string;
   text: string;
   icon: any;
+  url?: string;
+  className?: string;
+  onClick?: (event: any) => void;
 }
 export const SideMenuItem = (props: ISideMenuItemProps) => {
+  const anchourRender = () => {
+    return (
+      <a
+        className={`
+      flex flex-col 
+      justify-center 
+      items-center h-20 w-200
+       text-gray-600
+      ${props.className}
+      `}
+      >
+        {props.icon}
+        <span className={`text-xs font-light`}>{props.text}</span>
+      </a>
+    );
+  };
+
   return (
-    <li className={`hover:bg-gray-300`}>
-      <Link href={props.url}>
-        <a className={`flex flex-col justify-center items-center h-20 w-200`}>
-          {props.icon}
-          <span className={`text-xs font-light text-gray-600`}>{props.text}</span>
-        </a>
-      </Link>
+    <li
+      onClick={props.onClick}
+      className={`hover:bg-gray-300 cursor-pointer`}
+    >
+      {props.url ? (
+        <Link href={props.url}>{anchourRender()}</Link>
+      ) : (
+        anchourRender()
+      )}
     </li>
   );
 };
